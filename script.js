6d569309636a7b41035aa36aab8101f61b9b983d
Petezce <136310144+Petezce@users.noreply.github.com>
2023-06-17 12:17:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu_icons = document.querySelector('.menu-icons'),
+nav = document.querySelector('nav');
+
+menu_icons.addEventListener('click', () => {
+    nav.classList.toggle('active');
+}); 
+
